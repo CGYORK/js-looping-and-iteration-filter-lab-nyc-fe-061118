3fe810165425706750e84a8c7b1441026cb24865
Christian Georgeou <cgeorgeou@hotmail.com>
2018-07-26 00:43:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file
+
+function findMatching(drivers, names) {
+  return drivers.filter( drivers => drivers.toLowerCase() === names.toLowerCase());  
+}
+
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter( drivers => drivers.slice(0,letters.length) === letters); 
+}
+
+function matchName(drivers, names) {
+  return drivers.filter( drivers => drivers.name === names);  
+}
